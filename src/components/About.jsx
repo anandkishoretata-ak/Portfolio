4a10f3b275fc20c27323d58ba720f2f0a17a02bb
Tata Anand kishore <anandkishoretata@gmail.com>
2026-07-21 18:58:20 +0530

    my moditfy code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,28 @@
+import { motion } from "framer-motion";
+
 function About() {
   return (
-    <section id="about">
-        
+    <motion.section
+      id="about"
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+      viewport={{ once: true }}
+    >
       <h2>About Me</h2>
 
       <p>
-        I am a passionate MERN Stack Developer with experience
+           I am a passionate MERN Stack Developer with experience
         in building responsive and scalable web applications.
         I enjoy solving real-world problems through technology
         and continuously improving my development skills.
       </p>
-    </section>
+    </motion.section>
   );
 }
 
 export default About;
+ 
+       
+       
+
