7718c2f5cfb88b6b8af9d5d6ba250491e7c51d2a
Tata Anand kishore <anandkishoretata@gmail.com>
2026-07-21 19:02:24 +0530

    my moditfy code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,40 +1,80 @@
+
+import { motion } from "framer-motion";
+
 function Projects() {
   const projects = [
     {
       title: "Online Banking System",
       description:
         "Banking application with deposit, withdraw, transfer and transaction history.",
+      github:
+        "https://github.com/anandkishoretata-ak/Online-Banking-System.git",
     },
+    
     {
-      title: "Password Manager",
+      title: "Quiz Application",
       description:
-        "Secure password management application built with React.",
-    },
-    {
-      title: "Placement Management System",
-      description:
-        "Student placement tracking and management platform.",
+        "Quiz of programming language like java,python etc.",
+      github:
+        "https://github.com/anandkishoretata-ak/Quiz-Application.git",
     },
     {
       title: "MedTrack",
       description:
         "AWS-based healthcare management system.",
+      github:
+        "https://github.com/anandkishoretata-ak/medtrack.git",
+    },
+    {
+      title:"Placement Management System",
+      description:
+      "The students placements details",
+      github:
+      "https://github.com/anandkishoretata-ak/Placement-Management-System.git",
+
     },
   ];
 
   return (
-    <section id="projects">
+    <motion.section
+      id="projects"
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+      viewport={{ once: true }}
+    >
       <h2>Projects</h2>
 
       <div className="project-container">
         {projects.map((project, index) => (
-          <div key={index} className="project-card">
+          <motion.a
+            key={index}
+            href={project.github}
+            target="_blank"
+            rel="noreferrer"
+            className="project-card"
+            initial={{ opacity: 0, scale: 0.8 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.5,
+              delay: index * 0.2,
+            }}
+            viewport={{ once: true }}
+            whileHover={{
+              scale: 1.05,
+              y: -10,
+            }}
+          >
             <h3>{project.title}</h3>
             <p>{project.description}</p>
-          </div>
+
+            <span className="view-project">
+              🚀 View Project →
+            </span>
+          </motion.a>
         ))}
       </div>
-    </section>
+    </motion.section>
   );
 }
 
